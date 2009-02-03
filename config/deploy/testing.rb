2db12deb5c91d4ac37045b8051bd49568a98ab21
jameycribbs <jameycribbs@0e3fd41d-d72c-43e8-bc88-e5fa02bd3159>
2009-02-03 16:00:51 +0000

    added mongrel cluster config for testing
    git-svn-id: http://fusesource.com/forge/svn/fuseforge/rails/trunk@9 0e3fd41d-d72c-43e8-bc88-e5fa02bd3159

--- config/deploy/testing.rb
+++ config/deploy/testing.rb
@@ -1,5 +1,5 @@
 set :deploy_to, "/data/fuseforge"
-set :mongrel_conf, "#{current_path}/config/deploy/mongrel_cluster_production.yml"
+set :mongrel_conf, "#{current_path}/config/deploy/mongrel_cluster_testing.yml"
 set :rails_env, 'production'
 set :user, "rails"
 set :port, 30064
--- config/initializers/forum.rb
+++
@@ -1,2 +0,0 @@
-BROWSE_FORUM_URL = 'http://fusesource.com/forum/display'
-RECENT_FORUM_POSTS_LIMIT = 3
