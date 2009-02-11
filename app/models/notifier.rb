de52afabb53f6d76139c3ffb803c2330cacb70d5
jameycribbs <jameycribbs@0e3fd41d-d72c-43e8-bc88-e5fa02bd3159>
2009-02-11 21:59:34 +0000

    added email notification for project creation
    git-svn-id: http://fusesource.com/forge/svn/fuseforge/rails/trunk@129 0e3fd41d-d72c-43e8-bc88-e5fa02bd3159

--- app/models/notifier.rb
+++ app/models/notifier.rb
@@ -7,7 +7,7 @@ class Notifier < ActionMailer::Base
   end
 
   def project_creation_notification(project)
-    recipients SiteAdminGroup.users.collect { |u| "#{u.full_name} <#{u.email}>" }
+    recipients SiteAdminGroup.new.users.collect { |u| "#{u.full_name} <#{u.email}>" }
     from       FUSEFORGE_EMAIL_ADDRESS
     subject    "A new FUSE Forge project is awaiting approval."
     body       :project => project
