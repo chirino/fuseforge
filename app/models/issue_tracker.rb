43de3424e1f7b0851f1c8a292b17fe982881b774
sanjaymk <sanjaymk@0e3fd41d-d72c-43e8-bc88-e5fa02bd3159>
2009-02-14 20:33:16 +0000

    passing project description to the create project
    git-svn-id: http://fusesource.com/forge/svn/fuseforge/rails/trunk@169 0e3fd41d-d72c-43e8-bc88-e5fa02bd3159

--- app/models/issue_tracker.rb
+++ app/models/issue_tracker.rb
@@ -34,7 +34,8 @@ class IssueTracker < ActiveRecord::Base
   def create_internal
     return true if not use_internal? or exists_internally?
     is_pri = self.project.is_private?
-    JiraInterface.new.create_proj_default_perm(self.project.name, self.project.shortname, self.project.created_by.login, 
+    JiraInterface.new.create_proj_default_perm(self.project.name, self.project.shortname, self.project.description,
+      self.project.created_by.login, 
      JIRA_INTERNAL_URL,is_pri)
   end  
   
