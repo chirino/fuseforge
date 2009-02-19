70a78add6b6950c582d01740a01ff1821ac6c6e7
jameycribbs <jameycribbs@0e3fd41d-d72c-43e8-bc88-e5fa02bd3159>
2009-02-19 19:45:38 +0000

    fix for project downloads
    git-svn-id: http://fusesource.com/forge/svn/fuseforge/rails/trunk@191 0e3fd41d-d72c-43e8-bc88-e5fa02bd3159

--- app/controllers/download_requests_controller.rb
+++ app/controllers/download_requests_controller.rb
@@ -2,6 +2,9 @@ require 'net/http'
 require 'uri'
 
 class DownloadRequestsController < ApplicationController
+  before_filter :get_project
+  before_filter :set_return_to
+
   allow :user => :is_registered_user?, :redirect_to => '/'
 
   def create
@@ -17,15 +20,20 @@ class DownloadRequestsController < ApplicationController
     @download_request = DownloadRequest.new 
     @download_request.url = params[:url]
     @download_request.description = params[:desc]
-    @download_request.project = Project.find(params[:project_id])
-
-puts '-----------------------------------------'
-p @download_request
-p @download_request.valid?
-p @download_request.errors
-puts '-----------------------------------------' 
+    @download_request.project = @project
     @download_request.save
 
     redirect_to @download_request.url    
+  end
+
+  private
+  
+  def get_project
+    @project = Project.find(params[:project_id])
+  end
+  
+  def set_return_to
+    get_project
+    session[:return_to] = "#{FUSEFORGE_URL}/projects/#{@project.shortname}"
   end
 end
