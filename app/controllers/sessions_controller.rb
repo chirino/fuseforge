076a09185dafd7e172f026eb41186a3a6edd1f3a
jameycribbs <jameycribbs@0e3fd41d-d72c-43e8-bc88-e5fa02bd3159>
2009-02-19 18:36:37 +0000

    change to sessions new
    git-svn-id: http://fusesource.com/forge/svn/fuseforge/rails/trunk@187 0e3fd41d-d72c-43e8-bc88-e5fa02bd3159

--- app/controllers/sessions_controller.rb
+++ app/controllers/sessions_controller.rb
@@ -2,6 +2,8 @@ class SessionsController < ApplicationController
   skip_before_filter :login_required
 
   def new
+    store_location if session[:return_to].blank?
+    
     # send user to FUSESource login
     cookies[REDIRECT_BACK_COOKIE_NAME] = { :value => FUSEFORGE_URL + (session[:return_to].nil? ? '' : session[:return_to]), 
      :domain => REDIRECT_BACK_COOKIE_DOMAIN_NAME }
