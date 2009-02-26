dc519a2d975f2bfd7a38589b1fc10eacddda90cf
jameycribbs <jameycribbs@0e3fd41d-d72c-43e8-bc88-e5fa02bd3159>
2009-02-26 22:29:23 +0000

    fix for front page
    git-svn-id: http://fusesource.com/forge/svn/fuseforge/rails/trunk@236 0e3fd41d-d72c-43e8-bc88-e5fa02bd3159

--- app/controllers/admin_controller.rb
+++ app/controllers/admin_controller.rb
@@ -1,5 +1,5 @@
 class AdminController < ApplicationController
-  allow :user => :is_site_admin?, :redirect_to => url_for(:controller => 'homepage')
+  allow :user => :is_site_admin?, :redirect_to => { :controller => 'homepage' }
 
   def index
     respond_to do |format|
