7588e50a3b43764d4bee106e48e1d0c655ebb271
jameycribbs <jameycribbs@0e3fd41d-d72c-43e8-bc88-e5fa02bd3159>
2009-02-27 22:06:21 +0000

    added project name to notification email subjects
    git-svn-id: http://fusesource.com/forge/svn/fuseforge/rails/trunk@251 0e3fd41d-d72c-43e8-bc88-e5fa02bd3159

--- app/models/notifier.rb
+++ app/models/notifier.rb
@@ -2,14 +2,14 @@ class Notifier < ActionMailer::Base
   def project_approval_notification(project)
     recipients "#{project.created_by.full_name} <#{project.created_by.email}>"
     from       FUSEFORGE_EMAIL_ADDRESS
-    subject    "Your FUSE Forge project has been approved."
+    subject    "Your FUSE Forge project, #{project.name}, has been approved."
     body       :project => project
   end
 
   def project_creation_notification(project)
     recipients SiteAdminGroup.new.users.collect { |u| "#{u.full_name} <#{u.email}>" }
     from       FUSEFORGE_EMAIL_ADDRESS
-    subject    "A new FUSE Forge project is awaiting approval."
+    subject    "A new FUSE Forge project, #{project.name}, is awaiting approval."
     body       :project => project
   end
 end
