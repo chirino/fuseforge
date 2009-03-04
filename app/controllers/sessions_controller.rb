287c803a19cb2b91b18120e2d308cbff643becc1
jameycribbs <jameycribbs@0e3fd41d-d72c-43e8-bc88-e5fa02bd3159>
2009-03-04 22:53:12 +0000

    change to cookie domain
    git-svn-id: http://fusesource.com/forge/svn/fuseforge/rails/trunk@258 0e3fd41d-d72c-43e8-bc88-e5fa02bd3159

--- app/controllers/sessions_controller.rb
+++ app/controllers/sessions_controller.rb
@@ -3,8 +3,9 @@ class SessionsController < ApplicationController
 
   def new
     # send user to FUSESource login
-    cookies[REDIRECT_BACK_COOKIE_NAME] = { :value => FUSESOURCE_URL + (session[:return_to].nil? ? '' : session[:return_to]), 
-     :domain => REDIRECT_BACK_COOKIE_DOMAIN_NAME }
+#    cookies[REDIRECT_BACK_COOKIE_NAME] = { :value => FUSESOURCE_URL + (session[:return_to].nil? ? '' : session[:return_to]), 
+#     :domain => REDIRECT_BACK_COOKIE_DOMAIN_NAME }
+    cookies[REDIRECT_BACK_COOKIE_NAME] = { :value => FUSESOURCE_URL + (session[:return_to].nil? ? '' : session[:return_to]) }
     redirect_to "#{FUSESOURCE_URL}/login"
   end
 
@@ -12,7 +13,8 @@ class SessionsController < ApplicationController
     reset_session
 
     # send user to FUSESource logout
-    cookies[REDIRECT_BACK_COOKIE_NAME] = { :value => FUSESOURCE_URL, :domain => REDIRECT_BACK_COOKIE_DOMAIN_NAME } 
+#    cookies[REDIRECT_BACK_COOKIE_NAME] = { :value => FUSESOURCE_URL, :domain => REDIRECT_BACK_COOKIE_DOMAIN_NAME } 
+    cookies[REDIRECT_BACK_COOKIE_NAME] = { :value => FUSESOURCE_URL } 
     redirect_to "#{FUSESOURCE_URL}/logout"
   end
 end
