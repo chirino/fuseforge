d1f03c8259006ddbda5f72af88c95195c2144f5b
jameycribbs <jameycribbs@0e3fd41d-d72c-43e8-bc88-e5fa02bd3159>
2009-03-09 16:34:13 +0000

    fix for cookies
    git-svn-id: http://fusesource.com/forge/svn/fuseforge/rails/trunk@295 0e3fd41d-d72c-43e8-bc88-e5fa02bd3159

--- app/controllers/sessions_controller.rb
+++ app/controllers/sessions_controller.rb
@@ -2,6 +2,9 @@ class SessionsController < ApplicationController
   skip_before_filter :login_required
 
   def new
+logger.info '----------------- inside session new -------------------'
+logger.info session[:return_to].inspect
+logger.info '--------------------------------------------------------'    
     # send user to FUSESource login
     cookies[REDIRECT_BACK_COOKIE_NAME] = { :value => FUSEFORGE_URL + (session[:return_to].nil? ? '' : session[:return_to]), 
      :domain => REDIRECT_BACK_COOKIE_DOMAIN_NAME }
