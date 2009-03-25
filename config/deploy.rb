675f1d227fd911bb598c6167c04d0a64cbf4d542
jameycribbs <jameycribbs@0e3fd41d-d72c-43e8-bc88-e5fa02bd3159>
2009-03-25 18:03:50 +0000

    added delayed_job script
    git-svn-id: http://fusesource.com/forge/svn/fuseforge/rails/trunk@366 0e3fd41d-d72c-43e8-bc88-e5fa02bd3159

--- config/deploy.rb
+++ config/deploy.rb
@@ -61,17 +61,17 @@ end
 namespace :delayed_job do
   desc "Start delayed_job process" 
   task :start, :roles => :app do
-    run "cd #{current_path}; script/delayed_job start" 
+    invoke_command "cd #{current_path}; script/delayed_job start" 
   end
 
   desc "Stop delayed_job process" 
   task :stop, :roles => :app do
-    run "cd #{current_path}; script/delayed_job stop" 
+    invoke_command "cd #{current_path}; script/delayed_job stop" 
   end
 
   desc "Restart delayed_job process" 
   task :restart, :roles => :app do
-    run "cd #{current_path}; script/delayed_job restart" 
+    invoke_command "cd #{current_path}; script/delayed_job restart" 
   end
 end
 
