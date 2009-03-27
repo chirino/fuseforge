d002428e8c1b117f9a487b89e0133bd617d5401e
jameycribbs <jameycribbs@0e3fd41d-d72c-43e8-bc88-e5fa02bd3159>
2009-03-27 20:47:40 +0000

    changed members display to not show crowd group members
    git-svn-id: http://fusesource.com/forge/svn/fuseforge/rails/trunk@389 0e3fd41d-d72c-43e8-bc88-e5fa02bd3159

--- app/controllers/sessions_controller.rb
+++ app/controllers/sessions_controller.rb
@@ -3,6 +3,9 @@ class SessionsController < ApplicationController
   skip_before_filter :set_return_to
   
   def new
+    logger.info '--------------------------------'
+    logger.info request.env['HTTP_REFERRER'].inspect
+    logger.info '---------------------------------'
     session[:return_to] = request.env['HTTP_REFERER'] if request.env['HTTP_REFERER'] =~ /[\/forum|\/wiki]/
     redirect_to "#{FUSESOURCE_URL}/login?return_to=#{session[:return_to]}"
   end
