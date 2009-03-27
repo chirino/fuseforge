7d7d1a7143b2071b892ec005915bd976bbc70c1c
jameycribbs <jameycribbs@0e3fd41d-d72c-43e8-bc88-e5fa02bd3159>
2009-03-27 20:58:41 +0000

    changed members display to not show crowd group members
    git-svn-id: http://fusesource.com/forge/svn/fuseforge/rails/trunk@394 0e3fd41d-d72c-43e8-bc88-e5fa02bd3159

--- app/controllers/registrations_controller.rb
+++ app/controllers/registrations_controller.rb
@@ -3,7 +3,8 @@ class RegistrationsController < ApplicationController
   skip_before_filter :set_return_to
   
   def new
-    session[:return_to] = request.env['HTTP_REFERER'] if request.env['HTTP_REFERER'] =~ /[\/forum|\/wiki]/
+    session[:return_to] = request.env['HTTP_REFERER'] if request.env['HTTP_REFERER'].include?('/forum') or \
+     request.env['HTTP_REFERER'].include?('/wiki')
     redirect_to "#{FUSESOURCE_URL}/register?return_to=#{session[:return_to]}"
   end
 end
--- app/controllers/sessions_controller.rb
+++ app/controllers/sessions_controller.rb
@@ -3,12 +3,8 @@ class SessionsController < ApplicationController
   skip_before_filter :set_return_to
   
   def new
-    logger.info '--------------------------------'
-    logger.info request.env['HTTP_REFERER'].inspect
-    logger.info session[:return_to].inspect
-    session[:return_to] = request.env['HTTP_REFERER'] if request.env['HTTP_REFERER'] =~ /[\/forum|\/wiki]/
-    logger.info session[:return_to].inspect
-    logger.info '---------------------------------'
+    session[:return_to] = request.env['HTTP_REFERER'] if request.env['HTTP_REFERER'].include?('/forum') or \
+     request.env['HTTP_REFERER'].include?('/wiki')
     redirect_to "#{FUSESOURCE_URL}/login?return_to=#{session[:return_to]}"
   end
 
