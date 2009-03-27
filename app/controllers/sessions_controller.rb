2cb16c80b00bb819bf190161ac833baf260eeb09
jameycribbs <jameycribbs@0e3fd41d-d72c-43e8-bc88-e5fa02bd3159>
2009-03-27 20:20:39 +0000

    changed members display to not show crowd group members
    git-svn-id: http://fusesource.com/forge/svn/fuseforge/rails/trunk@385 0e3fd41d-d72c-43e8-bc88-e5fa02bd3159

--- app/controllers/sessions_controller.rb
+++ app/controllers/sessions_controller.rb
@@ -1,6 +1,5 @@
 class SessionsController < ApplicationController
   skip_before_filter :login_required
-  skip_before_filter :set_return_to
   
   def new
     redirect_to "#{FUSESOURCE_URL}/login?return_to=#{session[:return_to]}"
@@ -9,5 +8,11 @@ class SessionsController < ApplicationController
   def destroy
     reset_session
     redirect_to "#{FUSESOURCE_URL}/logout?return_to=#{FUSEFORGE_URL}"
+  end
+
+  private
+  
+  def set_return_to
+    session[:return_to] = request.env['HTTP_REFERER'] if request.env['HTTP_REFERER'].include?('/forum')
   end
 end
