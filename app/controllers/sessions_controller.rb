7b8c0f39f13dae31955c32e01ec0d23582dd6ee7
jameycribbs <jameycribbs@0e3fd41d-d72c-43e8-bc88-e5fa02bd3159>
2009-03-27 14:15:16 +0000

    changed how return to from fusesource is working
    git-svn-id: http://fusesource.com/forge/svn/fuseforge/rails/trunk@380 0e3fd41d-d72c-43e8-bc88-e5fa02bd3159

--- app/controllers/sessions_controller.rb
+++ app/controllers/sessions_controller.rb
@@ -3,16 +3,11 @@ class SessionsController < ApplicationController
   skip_before_filter :set_return_to
   
   def new
-#    unless session[:return_to].blank?
-#      cookies[REDIRECT_BACK_COOKIE_NAME] = { :value => FUSEFORGE_URL + session[:return_to], :domain => REDIRECT_BACK_COOKIE_DOMAIN_NAME }
-#      session[:return_to] = nil
-#    end
     redirect_to "#{FUSESOURCE_URL}/login?return_to=#{session[:return_to]}"
   end
 
   def destroy
-    return_to = session[:return_to]
     reset_session
-    redirect_to "#{FUSESOURCE_URL}/logout?return_to=#{return_to}"
+    redirect_to "#{FUSESOURCE_URL}/logout?return_to=#{FUSEFORGE_URL}"
   end
 end
