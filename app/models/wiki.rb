1445c2e63c11fb522f84f81eef10eae8352323a6
jameycribbs <jameycribbs@0e3fd41d-d72c-43e8-bc88-e5fa02bd3159>
2009-03-30 17:22:44 +0000

    added code to inactivate a project
    git-svn-id: http://fusesource.com/forge/svn/fuseforge/rails/trunk@405 0e3fd41d-d72c-43e8-bc88-e5fa02bd3159

--- app/models/wiki.rb
+++ app/models/wiki.rb
@@ -75,6 +75,7 @@ class Wiki < ActiveRecord::Base
   end
   
   def reset_permissions(reset_to_private)
+    confluence_interface = ConfluenceInterface.new
     confluence_interface.login
     confluence_interface.reset_space_perm(project.shortname, reset_to__private)
     confluence_interface.logout
