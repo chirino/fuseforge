2f6aaac2588b381de3a6a85943351171f7de6d31
jameycribbs <jameycribbs@0e3fd41d-d72c-43e8-bc88-e5fa02bd3159>
2009-04-03 14:55:42 +0000

    added registration code to send referring url
    git-svn-id: http://fusesource.com/forge/svn/fuseforge/rails/trunk@430 0e3fd41d-d72c-43e8-bc88-e5fa02bd3159

--- app/controllers/registrations_controller.rb
+++ app/controllers/registrations_controller.rb
@@ -3,12 +3,9 @@ class RegistrationsController < ApplicationController
   skip_before_filter :set_return_to
   
   def new
-    logger.info '-------------------------------------------'
-    logger.info request.env['HTTP_REFERER'].inspect
-    logger.info session[:return_to].inspect
-    logger.info '-------------------------------------------'
     session[:return_to] = request.env['HTTP_REFERER'] if request.env['HTTP_REFERER'].include?('/forum') or \
-     request.env['HTTP_REFERER'].include?('/wiki') or params[:clicked] == 'yes'
+     request.env['HTTP_REFERER'].include?('/wiki') or (params[:clicked] == 'yes' and not request.env['HTTP_REFERER'].include?(
+     '/activate')
     redirect_to "#{FUSESOURCE_URL}/register?return_to=#{session[:return_to]}"
   end
 end
