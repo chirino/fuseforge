669cd84a412c51ce82b726eed6544005ab8127c9
jameycribbs <jameycribbs@0e3fd41d-d72c-43e8-bc88-e5fa02bd3159>
2009-04-03 15:12:40 +0000

    added registration code to send referring url
    git-svn-id: http://fusesource.com/forge/svn/fuseforge/rails/trunk@432 0e3fd41d-d72c-43e8-bc88-e5fa02bd3159

--- app/controllers/registrations_controller.rb
+++ app/controllers/registrations_controller.rb
@@ -4,8 +4,7 @@ class RegistrationsController < ApplicationController
   
   def new
     session[:return_to] = request.env['HTTP_REFERER'] if request.env['HTTP_REFERER'].include?('/forum') or \
-     request.env['HTTP_REFERER'].include?('/wiki') or (params[:clicked] == 'yes' and not request.env['HTTP_REFERER'].include?(
-     '/activate'))
+     request.env['HTTP_REFERER'].include?('/wiki') or params[:clicked] == 'yes'
     redirect_to "#{FUSESOURCE_URL}/register?return_to=#{session[:return_to]}"
   end
 end
--- app/controllers/sessions_controller.rb
+++ app/controllers/sessions_controller.rb
@@ -4,7 +4,7 @@ class SessionsController < ApplicationController
   
   def new
     session[:return_to] = request.env['HTTP_REFERER'] if request.env['HTTP_REFERER'].include?('/forum') or \
-     request.env['HTTP_REFERER'].include?('/wiki')
+     request.env['HTTP_REFERER'].include?('/wiki') or params[:clicked] == 'yes'
     redirect_to "#{FUSESOURCE_URL}/login?return_to=#{session[:return_to]}"
   end
 
