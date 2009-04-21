1e3e17322a652d2b30455e5663ad774723a24f3f
chirino <chirino@0e3fd41d-d72c-43e8-bc88-e5fa02bd3159>
2009-04-21 15:32:23 +0000

    even simpler cap config
    git-svn-id: http://fusesource.com/forge/svn/fuseforge/rails/trunk@459 0e3fd41d-d72c-43e8-bc88-e5fa02bd3159

--- config/deploy/default.rb
+++ config/deploy/default.rb
@@ -1,9 +1,9 @@
 set :deploy_to, "/data/fuseforge"
-set :mongrel_conf, "#{current_path}/config/deploy/mongrel_cluster_default.yml"
-set :rails_env, 'production'
+set :mongrel_conf, "/data/fuseforge/shared/config/mongrel_cluster/mongrel_cluster.yml"
+set :rails_env, 'development'
 set :user, "rails"
 set :port, 22
-set :ssh_options, { :forward_agent => true, :keys => %w(/Users/chirino/sandbox/fuse/fuse-infra/images/common/root/.ssh/id_rsa)  }
+set :ssh_options, { :forward_agent => true }
 set :use_sudo, false
 
 role :app, "localhost"
