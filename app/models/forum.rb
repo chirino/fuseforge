603939e99f37a73e325be47f8c0c6757a33a416c
chirino <chirino@0e3fd41d-d72c-43e8-bc88-e5fa02bd3159>
2009-04-23 19:59:27 +0000

    - phpbb3 forum integration can now enabled/disabled via config. - better project edit form
    git-svn-id: http://fusesource.com/forge/svn/fuseforge/rails/trunk@476 0e3fd41d-d72c-43e8-bc88-e5fa02bd3159

--- app/models/forum.rb
+++ app/models/forum.rb
@@ -1,92 +1,131 @@
+#
+# By default the Forum model only supports external forums
+#
 class Forum < ActiveRecord::Base
-
   belongs_to :project
-  belongs_to :phpbb_forum, :class_name => "PhpbbForum", :foreign_key => "phpbb_forum_id"
   
   def before_save
     self.external_url = '' if use_internal?
   end
-  
-  def before_destroy
-    return true unless exists_internally?
+    
+  def internal_supported?
+    false
+  end  
 
-    phpbb_forum.lock
-    true
-  end
-  
-  def use_internal=(value)
-    bool_value = value == "1" ? true : false
-    
-    if (bool_value != read_attribute(:use_internal)) and not new_record?
-      if bool_value
-        create_internal
-      else
-        phpbb_forum.lock if exists_internally?
-      end    
-    end  
-    
-    write_attribute(:use_internal, value)
-  end  
-  
-  def change_name(value)
-    phpbb_forum.forum_name = value
-    phpbb_forum.save
-  end  
-  
-  def change_description(value)
-    phpbb_forum.forum_desc = value
-    phpbb_forum.save
+  def use_internal?
+    false
   end  
   
   def is_active?
-    use_internal? or not external_url.blank?
+    use_internal? || !external_url.blank?
   end
   
   def internal_url
-     "#{FORGE_URL}/forum/viewforum.php?f=#{phpbb_forum.forum_id}&start=0"    
+     nil
   end  
   
   def url
     use_internal? ? internal_url : external_url
   end
+
+end
+
+#
+# If the phpbb database is configured, then update the Forum model 
+# to allow internal phpbb3 forums.
+#
+if ActiveRecord::Base.configurations["phpbb"]
+
+  class Forum 
   
-  def exists_internally?
-    not phpbb_forum.blank?
-  end  
+    ActiveRecord::Base.belongs_to :phpbb_forum, :class_name => "PhpbbForum", :foreign_key => "phpbb_forum_id"
   
-  def create_internal
-    if use_internal?
-      if exists_internally?
-        phpbb_forum.unlock
-      else
-        self.phpbb_forum = PhpbbForum.create(:forum_name => project.name, :forum_desc => project.description)
-        save
-        phpbb_forum.create_group_rights
-      end
-      purge_cache
+    def internal_supported?
+      true
     end  
-  end
+
+    def use_internal?
+      read_attribute(:use_internal)
+    end  
   
-  def make_private
-    reset_permissions(true)
-  end
-  
-  def make_public
-    reset_permissions(false)
-  end    
-  
-  def reset_permissions(make_private)
-    return true if phpbb_forum.blank?
+    def use_internal=(value)
+      bool_value = value == "1" ? true : false
     
-    phpbb_forum.recreate_group_rights(make_private)
-    purge_cache
+      if (bool_value != read_attribute(:use_internal)) and not new_record?
+        if bool_value
+          create_internal
+        else
+          phpbb_forum.lock if exists_internally?
+        end    
+      end  
+    
+      write_attribute(:use_internal, value)
+    end  
+
+    def before_destroy
+      return true unless exists_internally?
+      phpbb_forum.lock
+      true
+    end
+  
+    def change_name(value)
+      phpbb_forum.forum_name = value
+      phpbb_forum.save
+    end  
+  
+    def change_description(value)
+      phpbb_forum.forum_desc = value
+      phpbb_forum.save
+    end  
+  
+    def internal_url
+       use_internal? ? "#{FORGE_URL}/forum/viewforum.php?f=#{phpbb_forum.forum_id}&start=0" : nil
+    end  
+  
+    def url
+      use_internal? ? internal_url : external_url
+    end
+  
+    def exists_internally?
+      not phpbb_forum.blank?
+    end  
+  
+    def create_internal
+      if use_internal?
+        if exists_internally?
+          phpbb_forum.unlock
+        else
+          self.phpbb_forum = PhpbbForum.create(:forum_name => project.name, :forum_desc => project.description)
+          save
+          phpbb_forum.create_group_rights
+        end
+        purge_cache
+      end  
+    end
+  
+    def make_private
+      reset_permissions(true)
+    end
+  
+    def make_public
+      reset_permissions(false)
+    end    
+  
+    def reset_permissions(make_private)
+      return true if phpbb_forum.blank?
+    
+      phpbb_forum.recreate_group_rights(make_private)
+      purge_cache
+    end
+  
+    def purge_cache
+      phpbb_forum.purge_cache
+    end
+  
+    def recent_posts
+      phpbb_forum.phpbb_posts.recent
+    end
+  
   end
   
-  def purge_cache
-    phpbb_forum.purge_cache
-  end
-  
-  def recent_posts
-    phpbb_forum.phpbb_posts.recent
-  end  
 end
