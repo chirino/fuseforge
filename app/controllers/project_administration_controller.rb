675b4e6d60bc445288e7476bcd6de9d66d1a2b2b
chirino <chirino@0e3fd41d-d72c-43e8-bc88-e5fa02bd3159>
2009-04-25 18:25:38 +0000

    Added tag admin page which supports removing tags
    git-svn-id: http://fusesource.com/forge/svn/fuseforge/rails/trunk@487 0e3fd41d-d72c-43e8-bc88-e5fa02bd3159

--- app/controllers/project_administration_controller.rb
+++ app/controllers/project_administration_controller.rb
@@ -1,17 +1,48 @@
 class ProjectAdministrationController < ApplicationController
-  before_filter :get_project
+  layout "new_look", :except => [:remove_tag, :add_tag, :auto_complete_for_tag]
+  before_filter :get_project, :except => [:auto_complete_for_tag]
 
   allow :user => :is_project_administrator_for?, :object => :project, :redirect_to => :homepage
-
+  protect_from_forgery :except => [:auto_complete_for_tag]
+  
   def index
     respond_to do |format|
-      format.html # index.html.erb
+      format.html
     end
   end
 
+  #
+  # Tag Editing...
+  #
+  def edit_tags
+    @project = Project.find(params[:id])
+  end
+  
+  def remove_tag
+    @project.tag_list.remove(params[:tag])
+    @project.save
+  	render( :partial => 'tags_editor' )
+  end
+
+  def add_tag
+  	@project.tag_list.add(params[:tag])
+  	@project.save
+  	render( :partial => 'tags_editor' )
+  end
+  
+  def auto_complete_for_tag
+    tag = h(params[:tag])
+    tag = '' unless tag
+    @regex = Regexp.new("(#{Regexp.escape(tag)})", true)
+    @tags = Project.tag_counts.select{|x| @regex.match(x.name) }.sort{|x,y| x.name<=>y.name}.first(10)
+    render(:partial => 'autocomplete_tags')
+  end
+  
   private
   
   def get_project
     @project = Project.find(params[:id])
   end
+
+
 end
--- vendor/plugins/auto_complete/lib/auto_complete_macros_helper.rb
+++ vendor/plugins/auto_complete/lib/auto_complete_macros_helper.rb
@@ -110,6 +110,13 @@ module AutoCompleteMacrosHelper
     content_tag("div", "", :id => "#{object}_#{method}_auto_complete", :class => "auto_complete") +
     auto_complete_field("#{object}_#{method}", { :url => { :action => "auto_complete_for_#{object}_#{method}" } }.update(completion_options))
   end
+
+  def text_field_tag_with_auto_complete(name, value, tag_options = {}, completion_options = {})
+    (completion_options[:skip_style] ? "" : auto_complete_stylesheet) +
+    text_field_tag(name, value, tag_options) +
+    content_tag("div", "", :id => "#{name}_auto_complete", :class => "auto_complete") +
+    auto_complete_field("#{name}", { :url => { :action => "auto_complete_for_#{name}" } }.update(completion_options))
+  end
   
   private
     def auto_complete_stylesheet
