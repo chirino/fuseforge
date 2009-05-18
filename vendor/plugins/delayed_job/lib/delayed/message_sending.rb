df505df9342a889c382a69b600292de318c97dcd
Hiram Chirino <hiram@hiramchirino.com>
2009-05-18 17:54:09 -0400

    Patched the delayed_job plugin so you can configure how long to delay running the job.

--- vendor/plugins/delayed_job/lib/delayed/message_sending.rb
+++ vendor/plugins/delayed_job/lib/delayed/message_sending.rb
@@ -1,9 +1,20 @@
 module Delayed
   module MessageSending
     def send_later(method, *args)
-      Delayed::Job.enqueue Delayed::PerformableMethod.new(self, method.to_sym, args)
+      priority = nil
+      run_at   = nil
+      case method
+      when String
+        method = method.to_sym
+      when Hash
+        priority = method[:priority]
+        run_at = method[:run_at]
+        args = method[:args] if method[:args]
+        method = method[:method].to_sym
+      end
+      Delayed::Job.enqueue Delayed::PerformableMethod.new(self, method, args), priority, run_at
     end
-    
+        
     module ClassMethods
       def handle_asynchronously(method)
         without_name = "#{method}_without_send_later"
