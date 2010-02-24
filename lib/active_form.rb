fe55d775235deea23bd897708112ab3d2766ce94
Hiram Chirino <hiram@hiramchirino.com>
2010-02-24 11:09:15 -0500

    Adding license headers.

--- lib/active_form.rb
+++ lib/active_form.rb
@@ -1,3 +1,20 @@
+# ===========================================================================
+# Copyright (C) 2009, Progress Software Corporation and/or its 
+# subsidiaries or affiliates.  All rights reserved.
+# 
+# Licensed under the Apache License, Version 2.0 (the "License");
+# you may not use this file except in compliance with the License.
+# You may obtain a copy of the License at
+# 
+#     http://www.apache.org/licenses/LICENSE-2.0
+# 
+# Unless required by applicable law or agreed to in writing, software
+# distributed under the License is distributed on an "AS IS" BASIS,
+# WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+# See the License for the specific language governing permissions and
+# limitations under the License.
+# ===========================================================================
+
 # This class allows one to leverage the goodness of the ActiveRecord class
 # without a database table having to back it up. Simply inherit this class
 # and add some columns like so:
